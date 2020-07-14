f59c3b4f234116f9e5f3b14af22aac53dbc40f10
Jody Eggleston <jmeggles@gmail.com>
2020-07-14 17:47:54 -0400

    added consoltable pkg and adjusted notes

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,23 @@
+--  this info is inserted into each table as a column with the repected value by name (employee, role, department).
+
 INSERT INTO employee (id, firstName, lastName, roleId, mgrId)
-VALUES (123, "John", "Doe", 1446337, 0221453)
+VALUES (123, "John", "Doe", 1446337, 0221453);
 
 INSERT INTO role (id, title, salary, deptId)
 VALUES (0221453, "Supervisor", 90159.25, 5);
 
 INSERT INTO department (id, deptName)
-VALUES ("5", "Operations");
+VALUES (5, "Operations");
 
-SELECT count(*) FROM authors;
+-- select returns the values
+SELECT count(*) FROM employee;
+
+
+
+
+
+-- ----------------------------------- --
+-- --------- notes ------------------
 -- ### Alternative way to insert more than one row
 -- INSERT INTO employee (nameEmpl, roleEmpl, department)
 -- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
